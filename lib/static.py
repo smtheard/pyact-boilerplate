64f7e76ed0c8d8cf317ad2b863b5bd1554370ab8
Stefan Theard <smtheard@gmail.com>
2016-09-29 00:53:26 -0500

    cleaner implementation for static routes

---
+++ lib/static.py
@@ -0,0 +1,17 @@
+import bottle
+
+@bottle.get('/<filename:re:.*\.js>')
+def javascripts(filename):
+  return bottle.static_file(filename, root='client/js')
+
+@bottle.get('/<filename:re:.*\.jsx>')
+def jsxs(filename):
+  return bottle.static_file(filename, root='client/jsx')
+
+@bottle.get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+  return bottle.static_file(filename, root='client/css')
+
+@bottle.get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+  return bottle.static_file(filename, root='client/img')
